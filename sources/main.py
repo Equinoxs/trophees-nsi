d3bda94581fced7ff4ae40d4caec0c2ce496aa23
Alexis Larose <larose.alexis@free.fr>
2024-10-23 13:50:28 +0200

    Write a base for SaveHandler and implement it

--- sources/main.py
+++ sources/main.py
@@ -1,15 +1,17 @@
 # Point d'entrée du jeu
 
-from src.classes.utils import GameLoop, TimeHandler, ControlHandler
+from src.classes.utils import GameLoop, TimeHandler, ControlHandler, SaveHandler
 
 control_handler = None
 time_handler = None
+save_handler = None
 
 
 def main():
     control_handler = ControlHandler()
     time_handler = TimeHandler()
-    game_loop = GameLoop(control_handler, time_handler)
+    save_handler = SaveHandler()
+    game_loop = GameLoop()
     return 0
 
 
---
+++ sources/src/classes/utils/save_handler/main.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+
+class SaveHandler:
+	def __init__(self):
+		self.default_save_path = os.path.join(
+			os.path.abspath(__file__),
+			'../../../../backups/new_game_backup.json'
+		)
+		return
+
+	def get_data_from_last_save(self):
+		path = os.path.join(
+			os.path.abspath(__file__),
+			'../../../../backups/automatic/main-save.json'
+		)
+		with open(path, 'r') as save:
+			data = json.load(save)
+		if data == {}:
+			with open(self.default_save_path, 'r') as save:
+				data = json.load(save)
+		return data
+
+	def save(self, data, backup_path):
+		path = path = os.path.join(
+			os.path.abspath(__file__),
+			'../../../../backups',
+   			backup_path
+		)
+		with open(path, 'w') as file:
+			json.dump(data, file, indent=4)
