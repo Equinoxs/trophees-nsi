35aafa2282149bd2eab90c0f66e096bcd56ec53b
Alexis Larose <larose.alexis@free.fr>
2024-10-23 13:50:28 +0200

    Write a base for SaveHandler and implement it

---
+++ sources/src/classes/specific/__init__.py
@@ -0,0 +1,4 @@
+# from .button.main import Button
+# from .mini_map.main import MiniMap
+from .player.main import Player
+# from .tree.main import Tree
--- sources/src/classes/utils/game_loop/main.py
+++ sources/src/classes/utils/game_loop/main.py
@@ -1,24 +1,29 @@
 import pygame
 
+from .....main import control_handler, time_handler, save_handler
+from ...specific import Player
 
 class GameLoop:
-	def __init__(self, control_handler, time_handler):
+	def __init__(self):
 		self.running = True
 		time_handler.set_clock(pygame.time.Clock())
 
 		pygame.init()
 		self.screen = pygame.display.set_mode((1280, 720))
+  
+		self.saved_data = save_handler.get_data_from_last_save()
+		self.player = Player(self.saved_data['player']['position'], self.saved_data['player']['image'], self.saved_data['player']['z_index'])
 
 		while self.running:
-			self.update(control_handler, time_handler)
+			self.update()
 			pygame.display.flip()  # Rafraîchit l'écran
 
 		pygame.quit()
 
-	def update(self, control_handler, time_handler):
+	def update(self):
 		control_handler.handleEvents(pygame)
 		if control_handler.is_activated('quit'):
 			self.running = False
 
 		time_handler.update()
-		# player.update() qui appellerait self.map.update() qui mettrait à jour tout le jeu
+		self.player.update()
