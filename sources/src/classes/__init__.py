f8b1cabe0ec4d1e5c3ebaa77c947afd25cdc24b9
Alexis Larose <larose.alexis@free.fr>
2024-10-24 13:30:22 +0200

    Add commentaries in src/classes/__init__.py file

--- sources/src/classes/__init__.py
+++ sources/src/classes/__init__.py
@@ -1,22 +1,31 @@
+# Il est important de mettre les classes dans l'ordre d'importation.
+# ex : si A importe B, alors on devra importer B avant A dans ce fichier
+# afin que B soit complètement initialisé quand A importera B
+
+# Classes n'important personne
 from .basics.vector_2.main import Vector2
 from .utils.control_handler.main import ControlHandler
 from .utils.save_handler.main import SaveHandler
 from .utils.time_handler.main import TimeHandler
 from .basics.map.main import Map
 
+# SoundMaker importe SoundTrack qui importe SoundMixer
 from .utils.sound_mixer.main import SoundMixer
 from .utils.sound_track.main import SoundTrack
 from .basics.sound_maker.main import SoundMaker
 
+# MapElement importe Animatable, Sprite et SoundMaker
 from .basics.sprite.main import Sprite
 from .basics.animatable.main import Animatable
 from .basics.map_element.main import MapElement
 
+# MapObject importe MapElement, Movable, Collider et Interactable
 from .basics.movable.main import Movable
 from .basics.collider.main import Collider
 from .basics.interactable.main import Interactable
 from .basics.map_object.main import MapObject
 
+# GameLoop importe Player qui importe MapObject
 from .specific.player.main import Player
 from .utils.game_loop.main import GameLoop
 
