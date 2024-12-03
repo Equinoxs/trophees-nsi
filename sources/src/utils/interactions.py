b5f9136c117d4595e10aae6d1ac3e09718a00a4f
Alexis Larose <larose.alexis@free.fr>
2024-12-03 20:16:29 +0100

    Fix side effect test function

--- sources/src/utils/interactions.py
+++ sources/src/utils/interactions.py
@@ -26,17 +26,21 @@ def npc_event_test(self, delta_time):
 
 # Les effets de bords :
 
-# Cet effet fait disparaître le pnj pendant 5 secondes si le joueur s'approche trop près
-# Une fois le pnj réapparu au bout des 5 secondes, il ne peut plus redisparaître
+# Cet effet fait disparaître le pnj pendant 3 secondes si le joueur s'approche trop près
+# Une fois le pnj réapparu au bout des 3 secondes, il ne peut plus redisparaître
 def npc_side_effect_test(self):
 	time = TimeHandler().add_chrono_tag('first_side_effect')
 	effect_made = self.side_effect_data('data_test')
-	if (self.position - GameLoop().get_player().get_focus().get_position()).get_norm() < 50 * GameLoop().get_camera().get_zoom() and effect_made != True:
-		self.set_z_index(-1)
-	if time > 4:
+ 
+	if not effect_made:
 		TimeHandler().remove_chrono_tag('first_side_effect')
-		self.side_effect_data('data_test', True)
+		if (self.position - GameLoop().get_player().get_focus().get_position()).get_norm() < 50 * GameLoop().get_camera().get_zoom() and not effect_made:
+			self.set_z_index(-1)
+			self.side_effect_data('data_test', True)
+
+	elif time > 3:
 		self.set_z_index(1)
+		TimeHandler().remove_chrono_tag('first_side_effect')
 
 interactions = {
 	"default": default,
