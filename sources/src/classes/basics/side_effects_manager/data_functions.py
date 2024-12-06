f576bb19171e5a80ccac8346a136b733fb543dfd
Alexis Larose <larose.alexis@free.fr>
2024-12-06 18:53:04 +0100

    Fix permanently pause system and finish a solid base for missions system

--- sources/src/classes/basics/side_effects_manager/data_functions.py
+++ sources/src/classes/basics/side_effects_manager/data_functions.py
@@ -1,22 +1,28 @@
-from src.classes import GameLoop, TimeHandler
+from src.classes import GameLoop, TimeHandler, LogHandler
 
 # Cet effet fait disparaître le pnj pendant 3 secondes si le joueur s'approche trop près
 # Une fois le pnj réapparu au bout des 3 secondes, il ne peut plus redisparaître
 def npc_side_effect_test(self):
 	if GameLoop().get_player().get_focus() == self:
 		return  # On ne veut pas que cet effet s'applique si on est le focus du Player
-	time = TimeHandler().add_chrono_tag('first_side_effect')
-	effect_made = self.side_effect_data('data_test')
- 
+
+	effect_made = self.side_effect_data('npc_side_effect_test_effect_made')
+	cleared = self.side_effect_data('npc_side_effect_test_cleared')
+	if not cleared:
+		time = TimeHandler().add_chrono_tag('first_side_effect')
+
 	if not effect_made:
 		TimeHandler().remove_chrono_tag('first_side_effect')
 		if (self.position - GameLoop().get_player().get_focus().get_position()).get_norm() < 50 * GameLoop().get_camera().get_zoom() and not effect_made:
 			self.set_z_index(-1)
-			self.side_effect_data('data_test', True)
+			LogHandler().add(f'{self.get_name()} * switch Z index to -1')
+			self.side_effect_data('npc_side_effect_test_effect_made', True)
 
-	elif time > 3:
-		self.set_z_index(1)
+	elif not cleared and time > 3:
+		LogHandler().add(f'{self.get_name()} * switch to the previous Z index')
+		self.set_z_index_prev()
 		TimeHandler().remove_chrono_tag('first_side_effect')
+		self.side_effect_data('npc_side_effect_test_cleared', True)
 
 
 side_effects = {
